68626c7d44bd541a39ddbd9c7a2bfae446d4cc7f
saraklemp <72497653+saraklemp@users.noreply.github.com>
2020-11-10 12:29:36 +0100

    Update 05_run_app.R

--- R/05_run_app.R
+++ R/05_run_app.R
@@ -5,18 +5,16 @@ library(leaflet)
 library(here)
 
 #### Connect R scripts -------------------------------------------------------
-setwd(here("R"))
-
-source("01_load_raw_data_and_clean.R",
+source(file = here("R", "01_load_raw_data_and_clean.R"),
        skip.echo = 2)
 
-source("02_define_variables.R",
+source(file = here("R", "02_define_variables.R"),
        skip.echo = 1)
 
-source("03_user_interface.R",
+source(file = here("R", "03_user_interface.R"),
        skip.echo = 2)
 
-source("04_server.R",
+source(file = here("R", "04_server.R"),
        skip.echo = 1)
 
 
