817a77bfa520888bd6e61e705c1a6ae3f2f4d0b6
saraklemp <72497653+saraklemp@users.noreply.github.com>
2020-11-10 12:02:39 +0100

    Update 05_run_app.R

--- R/05_run_app.R
+++ R/05_run_app.R
@@ -5,13 +5,17 @@ library(leaflet)
 library(dplyr)
 
 #### Connect R scripts -------------------------------------------------------
-
 source("01_load_raw_data_and_clean.R",
        skip.echo = 2)
 
 source("02_define_variables.R",
        skip.echo = 1)
 
+source("03_user_interface.R",
+       skip.echo = 2)
+
+source("04_server.R",
+       skip.echo = 1)
 
 
 #### App ---------------------------------------------------------------------
